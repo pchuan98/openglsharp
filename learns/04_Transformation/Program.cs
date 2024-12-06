c7e29ecb614cf0ad20ea1e0eb9365a642680c420
pchuan98 <haeer98@outlook.com>
2024-12-06 18:16:41 +0800

    feat: add mvp algorithm

--- learns/04_Transformation/Program.cs
+++ learns/04_Transformation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 using OpenGlSharp.Extensions;
 using OpenGlSharp.Helper;
 using OpenGlSharp.Models;
@@ -15,12 +16,10 @@ Log.Logger = new LoggerConfiguration()
                                      "{Message:lj}{NewLine}{Exception}")
     .CreateLogger();
 
-
-var window = new TransformationWindow();
+var window = new ViewTransformationWindow();
 window.Run();
 
-
-public class TransformationWindow : DemoWindow
+public class ModelTransformationWindow : DemoWindow
 {
     // set vbo and bannerTexture
     private static readonly float[] _vertext =
@@ -110,6 +109,102 @@ public class TransformationWindow : DemoWindow
         var scale = MathHelper.CreateScaleMain((float)Math.Sin(time), (float)Math.Sin(time));
 
         Shader?.UniformMatrix44("transform", rotate.Dot(scale));
+
+        Vao.Bind();
+
+        Gl.DrawElements(PrimitiveType.Triangles, 6, GLEnum.UnsignedInt, (void*)0);
+
+    }
+}
+
+public class ViewTransformationWindow : DemoWindow
+{
+    // set vbo and bannerTexture
+    private static readonly float[] _vertext =
+    [
+        1.5f,  1.5f, 0.0f,    1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
+        1.5f, -1.5f, 0.0f,  0.5f, 1.0f, 0.0f,  1.0f, 0.0f,
+       -1.5f, -1.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+       -3f,  1.5f, 0.0f,   0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+    ];
+
+    private static readonly uint[] Index = [0, 1, 2, 0, 3, 2];
+
+    private static readonly string _transVertexShader =
+        """
+        #version 330 core
+
+        layout(location=0) in vec3 aPos;
+        layout(location=2) in vec2 aSt;
+
+        out vec2 st;
+
+        uniform float time;
+        uniform mat4 transform;
+
+        void main(){
+            vec4 pos = vec4(aPos, 1.0);
+        
+           	gl_Position = transform * pos;
+        
+            st = aSt;
+        }
+        """;
+
+    private static readonly string _fragmentShader
+        = """
+          #version 330 core
+          in vec2 st;
+
+          out vec4 FragColor;
+          
+          uniform sampler2D sampler;
+
+          void main(){
+              FragColor = texture(sampler,st);
+          }
+          """;
+
+
+    public unsafe void LoadValue()
+    {
+        SetShader(_transVertexShader, _fragmentShader);
+
+        SetVbo(_vertext);
+        SetEbo(Index);
+
+        SetVao();
+        Vao.AddVertexAttributePointer(VertexAttribPointerType.Float, 8, 3, 0);
+        Vao.AddVertexAttributePointer(VertexAttribPointerType.Float, 8, 3, 3);
+        Vao.AddVertexAttributePointer(VertexAttribPointerType.Float, 8, 2, 6);
+
+        Texture = Texture.LoadFromFile(Gl, Path.Join(FileHelper.FindFolder("imgs"), "awesomeface.png"));
+    }
+
+    public override unsafe void Load()
+    {
+        base.Load();
+
+        LoadValue();
+    }
+
+    DateTime start = DateTime.Now;
+
+    public override unsafe void Render(double v)
+    {
+        base.Render(v);
+        var time = (float)((DateTime.Now - start).TotalSeconds);
+
+        Shader.Use();
+
+        Shader?.Uniform1("time", time);
+
+        Shader?.Uniform1("sampler", 0);
+
+        var model = MathHelper.CreateRotateY(time * 30);
+        var camera = MathHelper.CreateCameraMatrix([float.Sin(time), float.Cos(time), 0], [0, 0, 1], [0, 1, 0]);
+        var per = MathHelper.CreatePerspectiveProjection((float)Math.PI / 2f, 1.0f * WindowObj.Size.X / WindowObj.Size.Y, 1f, 1f, 10000f, 1f);
+        Shader?.UniformMatrix44("transform", per);
 
         Vao.Bind();
 
--- src/OpenGlSharp/Helper/MathHelper.cs
+++ src/OpenGlSharp/Helper/MathHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿// ReSharper disable InvalidXmlDocComment
+
+using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 using MathNet.Numerics.LinearAlgebra;
 
@@ -10,11 +12,6 @@ public static partial class MathHelper
     /// 角度转弧度
     /// </summary>
     internal const double RADIAN_SCALE = Math.PI / 180.0;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    internal static Vector3 OriginalPos = new(0, 0, 0);
 
     /// <summary>
     /// 
@@ -42,6 +39,37 @@ public static partial class MathHelper
     public static float[] To33(this float[] data)
     {
         throw new NotSupportedException();
+    }
+
+    /// <summary>
+    /// 向量减法
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static float[] Minus(this float[] a, float[] b)
+    {
+        if (a.Length != b.Length) throw new ArgumentException();
+
+        var count = a.Length;
+        var result = new float[count];
+
+        for (var i = 0; i < count; i++)
+            result[i] = a[i] - b[i];
+
+        return result;
+    }
+
+    /// <summary>
+    /// 任意向量变成单位向量
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <returns></returns>
+    public static float[] ToUnitVector(this float[] vector)
+    {
+        var sum = (float)Math.Sqrt(vector.Sum(i => i * i));
+
+        return vector.Select(item => item / sum).ToArray();
     }
 
     /// <summary>
@@ -102,7 +130,7 @@ partial class MathHelper
     }
 
     /// <summary>
-    /// 点乘
+    /// a . b 点乘
     /// </summary>
     /// <param name="a"></param>
     /// <param name="b"></param>
@@ -132,6 +160,37 @@ partial class MathHelper
                 result[row * 4 + col] = sum;
             }
         }
+    }
+
+    /// <summary>
+    /// 矩阵转置
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+    public static float[] T(this float[] matrix)
+    {
+        if (matrix.Length != 16)
+            throw new ArgumentException("must be a 4 x 4 matrix");
+
+        return [matrix[0], matrix[4], matrix[8], matrix[12],
+            matrix[1], matrix[5], matrix[9], matrix[13],
+            matrix[2], matrix[6], matrix[10], matrix[14],
+            matrix[3], matrix[7], matrix[11], matrix[15]];
+    }
+
+    /// <summary>
+    /// 向量a叉乘b（三维向量）
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static float[] Cross3(this float[] a, float[] b)
+    {
+        var x = a[1] * b[2] - a[2] * b[1];
+        var y = a[2] * b[0] - a[0] * b[2];
+        var z = a[0] * b[1] - a[0] * b[1];
+
+        return [x, y, z];
     }
 }
 
@@ -252,11 +311,7 @@ partial class MathHelper
     /// <returns></returns>
     public static float[] CreateScaleMain(float tx = 1, float ty = 1, float tz = 1)
         => [tx, 0, 0, 0, 0, ty, 0, 0, 0, 0, tz, 0, 0, 0, 0, 1];
-}
 
-// scale
-partial class MathHelper
-{
     /// <summary>
     /// 
     /// </summary>
@@ -291,7 +346,6 @@ partial class MathHelper
         var n33 = 1 + scale * uz * uz;
 
         return [n11, n12, n13, 0, n21, n22, n23, 0, n31, n32, n33, 0, 0, 0, 0, 1];
-
     }
 }
 
@@ -299,12 +353,184 @@ partial class MathHelper
 
 #region View / Camera Transformation
 
-// todo 
+partial class MathHelper
+{
+    /// <summary>
+    /// 创建相机（视图）变换矩阵
+    /// 
+    /// 理解方式为逆向移动物体，平移 -> 旋转
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="lookat">
+    /// 看的方向
+    /// </param>
+    /// <param name="top">
+    /// 相机的上方
+    /// </param>
+    /// <returns></returns>
+    public static float[] CreateCameraMatrix(float[] position, float[] lookat, float[] top)
+    {
+        lookat = lookat.ToUnitVector();
+        top = top.ToUnitVector();
+
+        // translate to origin
+        float[] origin = [
+            1, 0, 0, -position[0],
+            0, 1, 0, -position[1],
+            0, 0, 1, -position[2],
+            0, 0, 0, 1];
+
+        // inverse
+        var lx = lookat[0];
+        var ly = lookat[1];
+        var lz = lookat[2];
+
+        var tx = top[0];
+        var ty = top[1];
+        var tz = top[2];
+
+        var axisX = lookat.Cross3(top).ToUnitVector();
+        var ltx = axisX[0];
+        var lty = axisX[1];
+        var ltz = axisX[2];
+
+        /**
+         * note
+         * * 条件
+         * * 1. 相机朝向 -> l
+         * * 2. 相机上方 -> t
+         *
+         * 首先，可以利用l和t向量的叉乘得到需要的x
+         * 可以反向推导，
+         * 将-z变成l，将y变成t，求取逆矩阵
+         * 由于旋转矩阵是正交矩阵，所以矩阵的逆矩阵是转置
+         *
+         * | 1    0    0    0 |             | lx x tx    tx    -lx    0 |
+         * | 0    1    0    0 |      =>     | ly x ty    ty    -ly    0 |
+         * | 0    0    1    0 |      =>     | lz x tz    tz    -lz    0 |
+         * | 0    0    0    1 |             | 0          0     0      1 |
+         *
+         * 正交的充要条件： 当每一行 or 列 的结果都是单位向量且两两垂直
+         */
+
+        float[] rotate = [
+            ltx, tx, -lx, 0,
+            lty, ty, -ly, 0,
+            ltz, tz, -lz, 0,
+            0, 0, 0, 1];
+
+        var rotateT = rotate.T();
+
+        return rotateT.Dot(origin);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="target"></param>
+    /// <param name="top"></param>
+    /// <returns></returns>
+    public static float[] LookAt(float[] position, float[] target, float[] top)
+        => CreateCameraMatrix(position, target.Minus(position), top);
+}
 
 #endregion
 
 #region Projection Transformation
 
-// todo 
+partial class MathHelper
+{
+    /// <summary>
+    /// 正交投影 (结果变成-1到1)
+    /// </summary>
+    /// <param name="l"></param>
+    /// <param name="r"></param>
+    /// <param name="b"></param>
+    /// <param name="t"></param>
+    /// <param name="f"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static float[] CreateOrghographicProjection(
+        float l, float r, float b, float t, float f, float n)
+    {
+        // center cuboid by tranlating
+        var trans = CreateTranslation(-(l + r) / 2, -(t + b) / 2, -(f + n) / 2);
+
+        // scale into canonical cube
+        var scale = CreateScaleMain(2 / (r - l), 2 / (t - b), 2 / (n - f));
+
+        return scale.Dot(trans);
+    }
+
+    /**
+ * 关于透视投影
+ * 1. 近平面不变
+ * 2. 中心点不变
+ * 3. 近平面(n)和远平面(f)的z不变，中间的z会有所变化
+ * 4. f > n > 0
+ *
+ * 求解方法是 求解某个矩阵，将frustum（视锥体）压缩成为一个长方体，然后把这个长方体进行正交变换
+ *
+ * 通过相似三角形的关系，n的值，对任意z，有， (我们所期望的长方体应该是压缩到近平面的比例下)
+ * zx or zy = (nx or ny) * nz / zz
+ *  ==>
+ * | n    0    0    0 |  | x |      | xn |
+ * | 0    n    0    0 |  | y |  ==  | yn |
+ * | A    B    C    D |  | z |  ==  | ?  |
+ * | 0    0    0    z |  | 1 |      | z  |
+ * (ABCD)为未知待定系数
+ *
+ * 加入特值点
+ * * 在near上 任一点(i,j,n)变换结果不变
+ * * 在far上，任一点(k,l,f)变换结果中，z恒定f
+ *
+ * Ai + Bj + Cn + D = n*n
+ * Ak + Bl + Cf + D = f*f
+ *
+ * 使用特值法，当nf=0的时候 + ijkl为任意一点 => A = B = 0
+ *
+ * Cn + D = n*n
+ * Cf + D = f*f
+ * ==> C = n + f ,  D = -nf
+ */
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="l"></param>
+    /// <param name="r"></param>
+    /// <param name="b"></param>
+    /// <param name="t"></param>
+    /// <param name="f"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static float[] CreatePerspectiveProjection(
+        float l, float r, float b, float t, float f, float n)
+    {
+        //float[] trans = [
+        //    n, 0, 0, 0,
+        //    0, n, 0, 0,
+        //    0, 0, n + f, -n * f,
+        //    0, 0, 0, n];
+
+        //var ortho = CreateOrghographicProjection(l, r, b, t, f, n);
+        //var per = ortho.Dot(trans);
+
+
+        float[] per = [
+            (2 * n) / (r - l), 0, -(r + l) / (r - l), 0,
+            0, 2 * n / (t - b), -(t + b) / (t - b), 0,
+            0, 0, (n + f) / (n - f), -2 * n * f / (n - f),
+            0, 0, 1, 0];
+        //return per;
+        var matrix = Matrix4x4.CreatePerspectiveFieldOfView(l, r, n, f);
+
+        return [matrix.M11, matrix.M12, matrix.M13, matrix.M14,
+            matrix.M21, matrix.M22, matrix.M23, matrix.M24,
+            matrix.M31, matrix.M32, matrix.M33, matrix.M34,
+            matrix.M41, matrix.M42, matrix.M43, matrix.M44];
+    }
+}
 
 #endregion
